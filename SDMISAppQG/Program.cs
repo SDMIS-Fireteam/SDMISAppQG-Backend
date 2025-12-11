1c7597af4645b87866dedcf166f97b4549fba557
elliot-cx <derfire22@gmail.com>
2025-12-11 11:35:57 +0100

    feat/proper docker compose support

--- SDMISAppQG/Program.cs
+++ SDMISAppQG/Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using SDMISAppQG.Infrastructure.AppBuilder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services
    .AddNewtonsoftJson();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAppServices(builder.Configuration);
 
 var app = builder.Build();
 

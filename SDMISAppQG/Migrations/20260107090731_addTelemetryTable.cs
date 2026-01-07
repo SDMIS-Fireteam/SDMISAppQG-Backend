3f33da58edc192ebff1789f8ee954c6453d7e11b
Liam Lucas <liam.lucas693@gmail.com>
2026-01-07 10:11:24 +0100

    feat/add telemetry_log table

---
+++ SDMISAppQG/Migrations/20260107090731_addTelemetryTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+using SDMISAppQG.Models;
+
+#nullable disable
+
+namespace SDMISAppQG.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTelemetryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TelemetryLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VehicleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Position = table.Column<Point>(type: "geography(Point, 4326)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SensorsValues = table.Column<List<SensorValue>>(type: "jsonb", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelemetryLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelemetryLogs");
+        }
+    }
+}

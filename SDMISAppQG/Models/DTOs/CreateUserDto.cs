84009bd3ccb9c5a51a0cb721bbaeaccb68a66950
elliot-cx <derfire22@gmail.com>
2026-01-12 15:36:10 +0100

    test 2

--- SDMISAppQG/Models/DTOs/CreateUserDto.cs
+++ SDMISAppQG/Models/DTOs/CreateUserDto.cs
@@ -6,7 +6,7 @@ namespace SDMISAppQG.Models.DTOs;
 /// DTO pour la création d'un utilisateur
 /// </summary>
 public class CreateUserDto {
-   public Guid KeyCloakId { get; set; }
+   public required Guid KeyCloakId { get; set; }
    public required string Email { get; set; }
    public required string Username { get; set; }
    public required string Firstname { get; set; }
